23eb2f7260667483e0ab0b58b09e07f4b1ffbb4b
astronomiaview <40318633+astronomiaview@users.noreply.github.com>
2018-06-16 19:15:13 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
 
+    $(".navbar a[href^='#']").on('click', function(e) {
+       // store hash
+       var $el = (this.hash=='#top') ? $('body') : $(this.hash);
+        if ($el.length > 0) { 
+               // prevent default anchor click behavior
+               e.preventDefault();
+               // animate
+               $('html, body').animate({
+                   scrollTop: $el.offset().top
+                 }, 1000, function(){
+                   // when done, add hash to url
+                   // (default click behaviour)
+                   window.location.hash = this.hash;
+                 });
+        }
+    });
+});
+
+            
+            
             
             
